df77953797fa8569d1391e24cfa7ecbae296c69e
道化師 <wraith13@hotmail.co.jp>
2021-01-13 17:58:41 +0900

    🛠 minamo.core.comparer の実装
    https://github.com/wraith13/vscel/blob/master/source/comparer.ts からのコピペ

--- index.js
+++ index.js
@@ -380,6 +380,65 @@ var minamo;
             if (_ === void 0) { _ = null; }
             throw new Error("Not Yet Implement!");
         };
+        var comparer;
+        (function (comparer) {
+            comparer.basic = function (a, b) {
+                return a < b ? -1 :
+                    b < a ? 1 :
+                        0;
+            };
+            comparer.make = function (source) {
+                var _a;
+                var invoker = function (i) {
+                    var f = i;
+                    if ("function" === typeof f) {
+                        return function (a, b) { return comparer.basic(f(a), f(b)); };
+                    }
+                    var r = i;
+                    if (undefined !== (r === null || r === void 0 ? void 0 : r.raw)) {
+                        return r.raw;
+                    }
+                    var s = i;
+                    if (undefined !== (s === null || s === void 0 ? void 0 : s.getter)) {
+                        var body_1 = function (a, b) { return comparer.basic(s.getter(a), s.getter(b)); };
+                        if (undefined === s.condition) {
+                            return body_1;
+                        }
+                        else {
+                            var f_1 = s.condition;
+                            if ("function" === typeof f_1) {
+                                return function (a, b) { return f_1(a, b) ? body_1(a, b) : 0; };
+                            }
+                            else {
+                                var t_1 = s.condition;
+                                var getter_1 = t_1.getter;
+                                if (undefined === getter_1) {
+                                    return function (a, b) { return t_1.type === typeof a && t_1.type === typeof b ? body_1(a, b) : 0; };
+                                }
+                                else {
+                                    return function (a, b) { return t_1.type === typeof getter_1(a) && t_1.type === typeof getter_1(b) ? body_1(a, b) : 0; };
+                                }
+                            }
+                        }
+                    }
+                    return undefined;
+                };
+                if (Array.isArray(source)) {
+                    var comparerList_1 = source.map(invoker).filter(function (i) { return undefined !== i; });
+                    return function (a, b) {
+                        var result = 0;
+                        for (var i = 0; i < comparerList_1.length && 0 === result; ++i) {
+                            result = comparerList_1[i](a, b);
+                        }
+                        return result;
+                    };
+                }
+                else {
+                    return (_a = invoker(source)) !== null && _a !== void 0 ? _a : (function () { return 0; });
+                }
+            };
+            comparer.lowerCase = comparer.make([function (a) { return a.toLowerCase(); }, { raw: comparer.basic }]);
+        })(comparer = core_1.comparer || (core_1.comparer = {}));
     })(core = minamo.core || (minamo.core = {}));
     var environment;
     (function (environment) {
